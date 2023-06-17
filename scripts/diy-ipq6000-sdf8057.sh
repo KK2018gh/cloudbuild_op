7d8827de060ac5810cdf2c9a4e1ef6dc3d2d3aef
HoioH <67760304+KK2018gh@users.noreply.github.com>
2023-06-17 17:15:37 +0800

    Update diy-ipq6000-sdf8057.sh

--- scripts/diy-ipq6000-sdf8057.sh
+++ scripts/diy-ipq6000-sdf8057.sh
@@ -20,4 +20,6 @@
 #echo "src-git small https://github.com/kenzok8/small.git" >> "feeds.conf.default"
 echo "src-git alist https://github.com/alist-org/alist.git" >> "feeds.conf.default"
 echo "src-git luci_alist https://github.com/sbwml/luci-app-alist.git" >> "feeds.conf.default"
+echo "src-git ddns_go https://github.com/jeessy2/ddns-go.git" >> "feeds.conf.default"
+echo "src-git luci_ddns_go https://github.com/sirpdboy/luci-app-ddns-go.git" >> "feeds.conf.default"
 # echo "src-git autotimeset  https://github.com/sirpdboy/luci-app-autotimeset" >> "feeds.conf.default"
