39dcd769d8ed636f855b74be155d7eebaecc1ada
HoioH <67760304+KK2018gh@users.noreply.github.com>
2023-06-18 12:13:40 +0800

    Update diy-ipq6000-lean.sh

--- scripts/diy-ipq6000-lean.sh
+++ scripts/diy-ipq6000-lean.sh
@@ -16,8 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo "src-git kenzo https://github.com/kenzok8/small-package.git" >> "feeds.conf.default"
 echo "src-git small https://github.com/kenzok8/small.git" >> "feeds.conf.default"
+echo "src-git kenzo https://github.com/kenzok8/small-package.git" >> "feeds.conf.default"
 echo "src-git vlmcsd https://github.com/Wind4/vlmcsd.git" >> "feeds.conf.default"
 echo "src-git kms https://github.com/ssuperh/luci-app-vlmcsd-new.git" >> "feeds.conf.default"
 
