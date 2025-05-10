d4fe866b734b067e634063ef53359e6367fe4f37
HoioH <67760304+KK2018gh@users.noreply.github.com>
2025-05-10 20:16:53 +0800

    Update diy-im-op24.sh

--- scripts/diy-im-op24.sh
+++ scripts/diy-im-op24.sh
@@ -1,8 +1,8 @@
 # rm -rf feeds/packages/devel/gn
 # cp -rf feeds/smallpackage/gn feeds/packages/devel/gn
 
-# rm -rf feeds/packages/lang/golang
-# git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 rm -rf feeds/packages/net/sing-box
 cp -rf feeds/smallpackage/sing-box feeds/packages/net/sing-box
@@ -14,8 +14,8 @@ rm -rf feeds/packages/net/geoview
 cp -rf feeds/smallpackage/v2ray-geoview feeds/packages/net/geoview
 cp -rf feeds/smallpackage/v2ray-geoview feeds/packages/net/v2ray-geoview
 
-# rm -rf feeds/packages/net/xray-core
-# cp -rf feeds/smallpackage/xray-core feeds/packages/net/xray-core
+rm -rf feeds/packages/net/xray-core
+cp -rf feeds/smallpackage/xray-core feeds/packages/net/xray-core
 
 # rm -rf feeds/packages/net/miniupnpd
 # cp -rf feeds/smallpackage/miniupnpd feeds/packages/net/miniupnpd
