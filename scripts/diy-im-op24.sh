9b498e566f01dc5e41c6624b1b2c1b8a59538885
HoioH <67760304+KK2018gh@users.noreply.github.com>
2025-05-12 22:51:30 +0800

    Update diy-im-op24.sh

--- scripts/diy-im-op24.sh
+++ scripts/diy-im-op24.sh
@@ -13,6 +13,7 @@ cp -rf feeds/smallpackage/v2ray-geodata feeds/packages/net/v2ray-geodata
 # rm -rf feeds/packages/net/geoview
 # cp -rf feeds/smallpackage/v2ray-geoview feeds/packages/net/geoview
 # cp -rf feeds/smallpackage/v2ray-geoview feeds/packages/net/v2ray-geoview
+cp -rf feeds/smallpackage/v2ray-geoview feeds/packages/geoview
 
 rm -rf feeds/packages/net/xray-core
 cp -rf feeds/smallpackage/xray-core feeds/packages/net/xray-core
