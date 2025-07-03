3983a90e38aa55371e150d48c9f8c4ee23240ab9
HoioH <67760304+KK2018gh@users.noreply.github.com>
2025-07-03 13:42:36 +0800

    Update diy-immortalwrt.sh

--- scripts/diy-immortalwrt.sh
+++ scripts/diy-immortalwrt.sh
@@ -15,6 +15,7 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git golang https://github.com/KK2018gh/golang.git' >>feeds.conf.default
 echo 'src-git settings https://github.com/KK2018gh/cloudbuild_settings.git' >>feeds.conf.default
