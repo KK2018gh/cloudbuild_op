cbd9f45842bd11a6a35d011a142b7062ba10e3ff
HoioH <67760304+KK2018gh@users.noreply.github.com>
2025-07-28 06:29:34 +0800

    Update diy-im-op24.sh

--- scripts/diy-im-op24.sh
+++ scripts/diy-im-op24.sh
@@ -35,3 +35,5 @@ cp -rf feeds/smallpackage/xray-core feeds/packages/net/xray-core
 
 # rm -rf feeds/packages/net/miniupnpd
 # cp -rf feeds/smallpackage/miniupnpd feeds/packages/net/miniupnpd
+
+sed -i 's/192.168.1.1/192.168.6.51/g' package/base-files/files/bin/config_generate
