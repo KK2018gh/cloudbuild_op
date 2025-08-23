ec828ea7ea63e0c136113307aaabda7b3ece6178
HoioH <67760304+KK2018gh@users.noreply.github.com>
2025-08-24 05:42:59 +0800

    Update diy-im-op24.sh

--- scripts/diy-im-op24.sh
+++ scripts/diy-im-op24.sh
@@ -21,9 +21,8 @@ rm -rf feeds/packages/lang/golang
 cp -rf feeds/lean_packages/lang/golang feeds/packages/lang/golang
 rm -rf feeds/lean_packages
 
-git clone https://github.com/fw876/helloworld feeds/helloworld
 rm -rf feeds/luci/applications/luci-app-ssr-plus
-cp -rf feeds/helloworld/luci-app-ssr-plus feeds/luci/applications/luci-app-ssr-plus
+cp -rf feeds/smallpackage/luci-app-ssr-plus feeds/luci/applications/luci-app-ssr-plus
 
 rm -rf feeds/packages/net/sing-box
 cp -rf feeds/smallpackage/sing-box feeds/packages/net/sing-box
