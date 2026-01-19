ff298f4008ef039b9fcfc40d03a1ad5955ca72a5
HoioH <67760304+KK2018gh@users.noreply.github.com>
2026-01-19 22:32:39 +0800

    Update im-ipq-1.sh

--- scripts/im-ipq-1.sh
+++ scripts/im-ipq-1.sh
@@ -18,8 +18,8 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall.git' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
+echo 'src-git rtp2httpd https://github.com/stackia/rtp2httpd.git' >>feeds.conf.default
 # echo "src-git smallpackage https://github.com/KK2018gh/small-package.git" >> "feeds.conf.default"
 # echo "src-git small https://github.com/kenzok8/small.git" >> "feeds.conf.default"
 # echo 'src-git settings https://github.com/KK2018gh/cloudbuild_settings.git' >>feeds.conf.default
-echo 'src-git rtp2httpd https://github.com/stackia/rtp2httpd.git' >>feeds.conf.default
 # echo 'src-git aclp https://github.com/kingyond/luci-app-accesscontrol-plus.git' >>feeds.conf.default
