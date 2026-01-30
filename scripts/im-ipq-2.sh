bc395591d980f2375f61d5d6128e765a81c1f632
HoioH <67760304+KK2018gh@users.noreply.github.com>
2026-01-30 15:41:37 +0800

    Update im-ipq-2.sh

--- scripts/im-ipq-2.sh
+++ scripts/im-ipq-2.sh
@@ -8,17 +8,21 @@
 
 # 替换banner
 git clone https://github.com/KK2018gh/cloudbuild_settings.git feeds/settings
+
 rm -rf package/base-files/files/etc/banner
 cp -rf feeds/settings/immortal/banner package/base-files/files/etc/banner
 
+rm -rf package/feeds/packages/libffi/Makefile
+cp -rf feeds/settings/libffi/Makefile package/feeds/packages/libffi/Makefile
+
 # 替换OpenClash
-# rm -rf feeds/luci/applications/luci-app-openclash
-# cp -rf feeds/openclash/luci-app-openclash feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
+cp -rf feeds/openclash/luci-app-openclash feeds/luci/applications/luci-app-openclash
 
 # 替换passwall
 # git clone https://github.com/xiaorouji/openwrt-passwall feeds/passwall
-# rm -rf feeds/luci/applications/luci-app-passwall
-# cp -rf feeds/passwall/luci-app-passwall feeds/luci/applications/luci-app-passwall
+ rm -rf feeds/luci/applications/luci-app-passwall
+ cp -rf feeds/passwall/luci-app-passwall feeds/luci/applications/luci-app-passwall
 
 # 更新golang
 # git clone https://github.com/coolsnowwolf/packages feeds/lean_packages
@@ -41,17 +45,17 @@ cp -rf feeds/settings/immortal/banner package/base-files/files/etc/banner
 # smallpackage
 # git clone https://github.com/kenzok8/small-package.git feeds/smallpackage
 
-# rm -rf feeds/packages/net/sing-box
-# cp -rf feeds/smallpackage/sing-box feeds/packages/net/sing-box
+rm -rf feeds/packages/net/sing-box
+cp -rf feeds/smallpackage/sing-box feeds/packages/net/sing-box
 
-# rm -rf feeds/packages/net/hysteria
-# cp -rf feeds/smallpackage/hysteria feeds/packages/net/hysteria
+rm -rf feeds/packages/net/hysteria
+cp -rf feeds/smallpackage/hysteria feeds/packages/net/hysteria
 
 # rm -rf feeds/packages/net/v2ray-geodata
 # cp -rf feeds/smallpackage/v2ray-geodata feeds/packages/net/v2ray-geodata
 
-# rm -rf feeds/packages/net/geoview
-# cp -rf feeds/smallpackage/geoview feeds/packages/net/geoview
+rm -rf feeds/packages/net/geoview
+cp -rf feeds/smallpackage/geoview feeds/packages/net/geoview
 # cp -rf feeds/smallpackage/v2ray-geoview feeds/packages/net/v2ray-geoview
 # cp -rf feeds/smallpackage/v2ray-geoview feeds/packages/geoview
 
