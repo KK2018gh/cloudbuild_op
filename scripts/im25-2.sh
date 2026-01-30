40cdb235c9df97f0a0371b488a032ade0bcc6011
HoioH <67760304+KK2018gh@users.noreply.github.com>
2026-01-31 03:40:57 +0800

    Update im25-2.sh

--- scripts/im25-2.sh
+++ scripts/im25-2.sh
@@ -42,7 +42,7 @@ cp -rf feeds/openclash/luci-app-openclash feeds/luci/applications/luci-app-openc
 
 # smallpackage
 # rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-git clone https://github.com/kenzok8/small-package.git feeds/smallpackage
+# git clone https://github.com/kenzok8/small-package.git feeds/smallpackage
 # git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 
 rm -rf feeds/packages/net/sing-box
